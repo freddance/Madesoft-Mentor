ce4c475257498f33c245bf350cf99325bbb8956c
freddance <70547931+freddance@users.noreply.github.com>
2023-10-20 00:53:50 +0100

    first commit

---
+++ components/Student/OverViewCard.vue
@@ -0,0 +1,166 @@
+<template>
+    <div>
+      <div class="grid lg:grid-cols-4 gap-5 px-4 p-3 pb-5 mt-2">
+        <div class="overview-card-inner p-4  justify-between ">
+          <div class="">
+            <span class="sub-head font-extralight text-md">Total Students</span>
+            <h2 class="font-extrabold text-xl pt-2">120</h2>
+          </div>
+  
+          <div>
+            <div class="flex ">
+              <div class="pt-10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="114" height="10" viewBox="0 0 114 10" fill="none">
+                    <line opacity="0.3" y1="0.5" x2="114" y2="0.5" stroke="#0AA9CC"/>
+                    <path opacity="0.3" d="M0.5 4.5H47.5L57 9L61.5 4.5H113.5" stroke="#0AA9CC"/>
+                </svg>
+              </div>
+  
+              <div class="flex justify-end ml-auto pt-4"  >
+                <img src="~assets/images/total-student.svg" alt="" srcset="">
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        <div class="overview-card-inner   justify-between ">
+          <div class="p-4">
+            <span class="sub-head font-extralight text-md">New Students</span>
+            <h2 class="font-extrabold text-xl pt-2">15</h2>
+          </div>
+  
+          <div>
+            <div class="flex">
+              <div class=" increase flex h-6 w-16 pl-3 mx-8 mt-4">
+                56% 
+                <div class="pt-1.5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="12" viewBox="0 0 11 12" fill="none">
+                <path d="M3.20898 8.29183L7.79232 3.7085" stroke="#059669" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M3.20898 3.7085H7.79232V8.29183" stroke="#059669" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                </div>
+              </div>
+  
+              <div class="flex justify-end ml-auto pt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="121" height="51" viewBox="0 0 121 51" fill="none">
+<path d="M15.3152 37.0742C9.04973 38.9704 1 33.9219 1 33.9219V50.7487H121.746V2.14138C116.939 3.74459 109.025 0.28355 105.077 5.14178C93.2638 19.6814 84.6558 3.30006 74.686 7.29544C64.5141 11.3719 61.6826 18.4412 50.5368 20.2988C45.532 21.133 48.6792 32.4936 43.1063 33.9219C37.5334 35.3501 33.1989 24.0145 28.2452 23.3953C22.6637 22.6976 20.6163 35.4698 15.3152 37.0742Z" fill="url(#paint0_linear_2072_2280)"/>
+<path d="M1 34.3176C1 34.3176 11.4714 40.1286 17.0995 36.3985C21.4339 33.5258 22.147 25.7022 27.626 23.3951C30.7221 22.0914 37.4146 34.6252 42.8276 34.2662C48.129 33.9147 47.0216 21.2722 49.0197 20.8669C54.8713 19.6798 59.3346 17.6175 73.4476 7.91486C83.3549 1.10354 93.8824 22.1574 105.647 4.19948C106.863 2.34263 119.269 2.76129 121.746 1.52295" stroke="#059669" stroke-width="1.23842" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<linearGradient id="paint0_linear_2072_2280" x1="67.1644" y1="-2.09718" x2="67.1644" y2="49.8491" gradientUnits="userSpaceOnUse">
+<stop stop-color="#03C08E"/>
+<stop offset="1" stop-color="#F1EDFF" stop-opacity="0"/>
+</linearGradient>
+</defs>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+        <div class="overview-card-inner   justify-between ">
+          <div class="p-4">
+            <span class="sub-head font-extralight text-md">Active Students</span>
+            <h2 class="font-extrabold text-xl pt-2">15</h2>
+          </div>
+  
+          <div>
+            <div class="flex">
+              <div class=" increase flex h-6 w-16 pl-3 mx-8 mt-4">
+                78% 
+                <div class="pt-1.5">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="11" height="12" viewBox="0 0 11 12" fill="none">
+                <path d="M3.20898 8.29183L7.79232 3.7085" stroke="#059669" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                <path d="M3.20898 3.7085H7.79232V8.29183" stroke="#059669" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                </div>
+              </div>
+  
+              <div class="flex justify-end ml-auto pt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="121" height="51" viewBox="0 0 121 51" fill="none">
+<path d="M15.3152 37.0742C9.04973 38.9704 1 33.9219 1 33.9219V50.7487H121.746V2.14138C116.939 3.74459 109.025 0.28355 105.077 5.14178C93.2638 19.6814 84.6558 3.30006 74.686 7.29544C64.5141 11.3719 61.6826 18.4412 50.5368 20.2988C45.532 21.133 48.6792 32.4936 43.1063 33.9219C37.5334 35.3501 33.1989 24.0145 28.2452 23.3953C22.6637 22.6976 20.6163 35.4698 15.3152 37.0742Z" fill="url(#paint0_linear_2072_2280)"/>
+<path d="M1 34.3176C1 34.3176 11.4714 40.1286 17.0995 36.3985C21.4339 33.5258 22.147 25.7022 27.626 23.3951C30.7221 22.0914 37.4146 34.6252 42.8276 34.2662C48.129 33.9147 47.0216 21.2722 49.0197 20.8669C54.8713 19.6798 59.3346 17.6175 73.4476 7.91486C83.3549 1.10354 93.8824 22.1574 105.647 4.19948C106.863 2.34263 119.269 2.76129 121.746 1.52295" stroke="#059669" stroke-width="1.23842" stroke-linecap="round" stroke-linejoin="round"/>
+<defs>
+<linearGradient id="paint0_linear_2072_2280" x1="67.1644" y1="-2.09718" x2="67.1644" y2="49.8491" gradientUnits="userSpaceOnUse">
+<stop stop-color="#03C08E"/>
+<stop offset="1" stop-color="#F1EDFF" stop-opacity="0"/>
+</linearGradient>
+</defs>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="overview-card-inner   justify-between ">
+          <div class="p-4">
+            <span class="sub-head font-extralight text-md">Dormant Students</span>
+            <h2 class="font-extrabold text-xl pt-2">22</h2>
+          </div>
+  
+          <div>
+            <div class="flex">
+              <div class=" decrease flex h-6 w-16 pl-2 mx-8 mt-4">
+                12% 
+                <div class="pt-1.5">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="11" height="12" viewBox="0 0 11 12" fill="none">
+                  <path d="M7.79232 3.7085L3.20898 8.29183" stroke="#F80707" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                  <path d="M7.79232 8.29183H3.20898V3.7085" stroke="#F80707" stroke-width="1.375" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                </div>
+              </div>
+  
+              <div class="flex justify-end ml-auto pt-2">
+                <svg xmlns="http://www.w3.org/2000/svg" width="130" height="54" viewBox="0 0 130 54" fill="none">
+                <path d="M18.1346 38.9668C11.5106 40.9715 0.642578 41.2876 0.642578 41.2876L0.642578 53.4237L130.655 53.4237V2.03527C125.573 3.73021 117.206 0.0711379 113.032 5.20734C100.543 20.5788 91.4426 3.26024 80.9024 7.48422C70.1484 11.7939 67.1549 19.2677 55.3714 21.2316C50.0803 22.1135 53.4075 34.1241 47.5158 35.6341C41.624 37.1441 37.0415 25.1599 31.8044 24.5052C25.9035 23.7676 23.739 37.2707 18.1346 38.9668Z" fill="url(#paint0_linear_2072_2668)"/>
+                <path d="M1.95117 41.2875C1.95117 41.2875 14.0702 42.1959 20.0202 38.2524C24.6027 35.2152 25.3566 26.9441 31.1491 24.505C34.4223 23.1267 41.4978 36.3776 47.2205 35.9981C52.8251 35.6264 51.6545 22.2606 53.7669 21.8321C59.9532 20.5771 64.672 18.3968 79.5924 8.13897C90.0666 0.937941 101.196 23.1964 113.634 4.21101C114.92 2.24792 127.642 2.66368 130.26 1.35449" stroke="#F80707" stroke-width="1.30928" stroke-linecap="round" stroke-linejoin="round"/>
+                <defs>
+                <linearGradient id="paint0_linear_2072_2668" x1="72.9504" y1="-2.44581" x2="72.9504" y2="52.4727" gradientUnits="userSpaceOnUse">
+                <stop stop-color="#FF8795"/>
+                <stop offset="1" stop-color="#F1EDFF" stop-opacity="0"/>
+                </linearGradient>
+                </defs>
+                </svg>
+              </div>
+            </div>
+          </div>
+        </div>
+  
+      </div>
+  
+  
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style>
+  .overview-card {
+    background: #FFFFFF;
+    border: 1px solid #EAECF0;
+    box-shadow: 0px 0.5px 3px rgba(16, 24, 40, 0.1), 0px 0.4px 2px rgba(16, 24, 40, 0.06);
+    border-radius: 8px;
+  }
+  
+  .overview-card-inner {
+    border-radius: 4px;
+    background: var(--shades-white, #FFF);
+    box-shadow: 0px 4px 10px 0px rgba(0, 0, 0, 0.10);
+  }
+
+
+
+  .increase {
+    border-radius: 14px;
+background: rgba(2, 125, 29, 0.05);
+color: #059669;
+  }
+
+  .decrease {
+    background: #FEE;
+    color: #F80707;
+    border-radius: 14px;
+  }
+  
+  </style>
